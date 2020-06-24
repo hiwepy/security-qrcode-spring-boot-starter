91567675e4ab86ff936c15a7bc8e540a0cbec399
wandl <hnxyhcwdl1003@163.com>
2020-06-24 13:30:46 +0800

    yh

--- src/main/java/org/springframework/security/boot/SecurityQrcodeFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityQrcodeFilterConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityQrcodeFilterConfiguration {
 	@Configuration
 	@EnableConfigurationProperties({ SecurityQrcodeProperties.class, SecurityQrcodeAuthzProperties.class, SecurityBizProperties.class })
 	@Order(SecurityProperties.DEFAULT_FILTER_ORDER + 2)
-	static class QrcodeWebSecurityConfigurerAdapter extends AbstractSecurityConfigurerAdapter {
+	static class QrcodeWebSecurityConfigurerAdapter extends WebSecurityBizConfigurerAdapter {
 
 	    private final SecurityQrcodeAuthzProperties authcProperties;
 	    
