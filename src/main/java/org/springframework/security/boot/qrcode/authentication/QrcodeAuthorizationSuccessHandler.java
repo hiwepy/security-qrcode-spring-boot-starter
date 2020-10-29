01a260de6a45c6ae01e4907e1a59a3efd2004fb3
wandl <hiwepy@163.com>
2020-10-29 10:16:51 +0800

    优化依赖

--- src/main/java/org/springframework/security/boot/qrcode/authentication/QrcodeAuthorizationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/qrcode/authentication/QrcodeAuthorizationSuccessHandler.java
@@ -76,7 +76,7 @@ public class QrcodeAuthorizationSuccessHandler implements AuthenticationSuccessH
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		// 国际化后的异常信息
-		String message = messages.getMessage(AuthResponseCode.SC_AUTHC_SUCCESS.getMsgKey());
+		String message = messages.getMessage(AuthResponseCode.SC_SUCCESS.getMsgKey());
 		// 写出JSON
 		JSONObject.writeJSONString(response.getWriter(), AuthResponse.success(message, profilePayload));
 		
