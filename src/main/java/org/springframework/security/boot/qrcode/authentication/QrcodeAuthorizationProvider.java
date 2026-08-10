8eb3d1d180894132d6d176911900e90780999eb0
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:02:20 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/qrcode/authentication/QrcodeAuthorizationProvider.java
+++ src/main/java/org/springframework/security/boot/qrcode/authentication/QrcodeAuthorizationProvider.java
@@ -26,7 +26,7 @@ import java.util.Set;
 /**
  *
  * Jwt授权 (authorization)处理器
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class QrcodeAuthorizationProvider implements AuthenticationProvider {
 
@@ -46,7 +46,7 @@ public class QrcodeAuthorizationProvider implements AuthenticationProvider {
     /**
      *
      * 完成匹配Token的认证，这里返回的对象最终会通过：SecurityContextHolder.getContext().setAuthentication(authResult); 放置在上下文中
-     * @author [@Loong Wan](https://github.com/loong10k)
+     * @author <a href="https://github.com/loong10k">Loong Wan</a>
      * @param authentication  {@link QrcodeAuthorizationToken} 对象
      * @return 认证结果{@link QrcodeAuthorizationToken}对象
      * @throws AuthenticationException 认证失败会抛出异常
