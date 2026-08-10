e2aa3ebcc9576fe82b864faf4f3d8e0b6b12fbb6
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:02:17 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/qrcode/userdetails/QrcodeInfo.java
+++ src/main/java/org/springframework/security/boot/qrcode/userdetails/QrcodeInfo.java
@@ -19,7 +19,7 @@ import lombok.Data;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Data
 public class QrcodeInfo {
